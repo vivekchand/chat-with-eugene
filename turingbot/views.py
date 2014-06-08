3b9072f9f169df174b93fd55122093b8887fd864
Vivek Chand <vivekchand19@gmail.com>
2014-06-08 21:24:51 +0530

    code cleanup

--- turingbot/views.py
+++ turingbot/views.py
@@ -7,12 +7,13 @@ from django.shortcuts import get_object_or_404, render
 def index(request):
     return render(request, 'index.html')
 
+
 def ask(request):
-	question = request.GET.get('question')
-	try:
-		response = requests.post('http://www.princetonai.com/bot/botanswer.do'
-			'?request={}'.format(question))
-		message = response.content
-	except:
-		message = "I'm a little busy now, will ping you later :)"
-	return HttpResponse(message)
+    question = request.GET.get('question')
+    try:
+        response = requests.post('http://www.princetonai.com/bot/botanswer.do'
+                                 '?request={}'.format(question))
+        message = response.content
+    except:
+        message = "I'm a little busy now, will ping you later :)"
+    return HttpResponse(message)
