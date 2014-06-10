1244089f70c52f66f332428fd0004ac7e56be984
Vivek Chand <vivekchand19@gmail.com>
2014-06-10 20:39:25 +0530

    encode question to utf-8

--- turingbot/views.py
+++ turingbot/views.py
@@ -12,7 +12,7 @@ def ask(request):
     question = request.GET.get('question')
     try:
         response = requests.post('http://default-environment-sdqm3mrmp4.elasticbeanstalk.com/AWSBot'
-                                 '?request={}'.format(question))
+                                 '?request={}'.format(question.encode('utf-8')))
         message = response.content
     except:
         message = "I'm a little busy now, will ping you later :)"
